d10e16473e64c3800031684bd9e2e6c4e788c85d
LaRothere <71266024+LaRothere@users.noreply.github.com>
2022-12-12 18:05:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,55 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+from datetime import datetime,date,time,timezone,timedelta
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import pandas as pd
+import numpy as np
+import pytz
+from matplotlib import pyplot as plt
+from sqlalchemy import create_engine #to access a sql database
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# pd.read_csv(r"G:\My Drive\Madrid\COURS\TBDA\TBDATEAM\data.csv")
+# uploaded_file = st.file_uploader("Fill out the project plan template and upload your file here. After you upload the file, you can edit your project plan within the app.", type=['csv'])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+engine = create_engine('postgresql://lectura:ncorrea#2022@138.100.82.178:5432/2207')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Recuperation of all operating modes
+OPmodes = pd.read_sql_query("select id_var, value from variable_log_float where id_var=622 limit 1000", con=engine)
+list1=OPmodes["value"]
+x= np.array(list1)
+op_modes = np.unique(x).astype(str)[:-1]
+def query(x):
+    return pd.read_sql_query("".join(["select id_var, date, to_timestamp(@date/1000) as dateH, value from variable_log_float where id_var=622 and value=",x]),con=engine)
+modes = [query(x) for x in op_modes]
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.title("Histogram per operating mode")
+
+# Choice of the time window
+start_date = st.date_input("Start date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
+start_time = st.time_input("Start time",time(6,00,00))
+end_date = st.date_input("End date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
+end_time = st.time_input("End time",time(22,00,00))
+
+start_datetime = datetime.combine(start_date,start_time,tzinfo=timezone(timedelta(seconds=3600)))
+end_datetime = datetime.combine(end_date,end_time,tzinfo=timezone(timedelta(seconds=3600)))
+
+#Number of minutes in the interval
+time_interval = end_datetime - start_datetime
+time_interval_min = time_interval.seconds/60
+
+
+# operations collected and operations per minute
+counters_collected = []
+counters_per_min = []
+
+
+for mode in modes:
+    op_collected = mode[mode['dateh'] > start_datetime][mode['dateh'] < end_datetime]['dateh'].size
+    counters_collected.append(op_collected)
+    counters_per_min.append(op_collected/time_interval_min)
+
+st.text("Collected operations by operating mode")
+st.bar_chart(counters_collected)
+
+st.text("Collected operations per minute by operating mode")
+st.bar_chart(counters_per_min)
